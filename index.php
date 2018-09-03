a78f98a67542f862c231238156a2032268d6eb6d
fadikamohamed <fadika.mohamed02@gmail.com>
2018-09-03 14:59:37 +0200

    exercice terminé

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="utf-8" />
+    <title>Exercice 2</title>
+  </head>
+  <body>
+    <p>
+    <?php
+      function stringFunction($lien) {
+        echo $lien;
+      }
+      stringFunction('youhou');
+     ?>
+   </p>
+  </body>
+</html>
